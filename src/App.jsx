10b583c1ef510261cd6e6c6e5cf818db731cda14
tkiyo86 <32362106+tkiyo86@users.noreply.github.com>
2021-07-07 11:29:20 +0900

    54_button_delete 削除ボタンの機能実装 map関数のindexをつかってTODOリストの何行目を削除するか判断する関数を組む 次回は完了ボタン

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 
-// 52, 入力内容は可変＝Stateとして定義、いったん初期値をおく
-// useStateで入力値を準備して、Map関数で処理する
-//  key={todo} を忘れないように、どの処理をするか把握するための目印？
+// 54 削除ボタンの機能実装
+// JS実装では削除したい行のdivを取得していた
+// ReactではincompleteTodos　からテキストを削除する？
 export const App = () => {
   // 53 ボタン機能実装
   const [todoText, setTodoText] = useState("");
   const onChangeTodoText = (event) => setTodoText(event.target.value);
+
   const onClickAdd = () => {
     if (todoText === "") return;
     // 入力内容を受け取る配列、incompleteTodos　に　todoTextを追加した配列が完成
@@ -17,6 +18,18 @@ export const App = () => {
     // 上段の入力内容を消す＝空文字入力
     setTodoText("");
   };
+  //54 削除ボタンの機能実装、どの行を押したかわかるように
+  // incompleteTodos.map((todo, index) でindexにループ番号が入る
+  // () => onClickDelete(index)  関数に引数を渡したい時はアロー関数で書かないと勝手にアラート実行される
+  const onClickDelete = (index) => {
+    // alert(index + ":削除");
+    // incompleteTodos を使ってコピー
+    const newTodos = [...incompleteTodos];
+    // spliceで　index行目から引数２の数だけ削除
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
   // 52　 Reactに対応したmock
   const [incompleteTodos, setIncompleteTodos] = useState(["sample", "sample2"]);
   const [completeTodos, setcompleteTodos] = useState(["sample3"]);
@@ -33,12 +46,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のToDo</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
